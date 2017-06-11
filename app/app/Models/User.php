41010c5d7348fdada5bde5a2292a4cf207f24f5f
Patrick Foh <patrickfoh@gmail.com>
2017-06-11 22:21:59 +0100

    Updated User model

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Models\Traits\Uuids;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Uuids;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
@@ -26,4 +34,19 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function subscription()
+    {
+      return $this->hasOne('App\Models\Subscription');
+    }
+
+    public function roles()
+    {
+      return $this->belongsToMany('App\Models\Role', 'user_roles');
+    }
+
+    public function orgs()
+    {
+      return $this->belongsToMany('App\Models\Org', 'org_users');
+    }
 }
