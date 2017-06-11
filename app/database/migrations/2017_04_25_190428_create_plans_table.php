9789c675d4f5fe5a50a4698aaa15627be423aa2d
Patrick Foh <patrickfoh@gmail.com>
2017-06-11 18:27:52 +0100

    Fixed migration typo error

--- app/database/migrations/2017_04_25_190428_create_plans_table.php
+++ app/database/migrations/2017_04_25_190428_create_plans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSubscriptionsPlansTable extends Migration
+class CreatePlansTable extends Migration
 {
     /**
      * Run the migrations.
