5f9fe5d027129891a4f0c0999041fb690c6b3895
Patrick Foh <patrickfoh@gmail.com>
2017-06-14 09:15:06 +0100

    Added Requests

---
+++ app/app/Http/Requests/V1/CreateUser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUser extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8',
+            'org_name' => 'unique:orgs,name'
+        ];
+    }
+}
