7aea8538bbf8f5486b91f16727b2fd77c5c08849
Patrick Foh <patrickfoh@gmail.com>
2017-06-14 09:13:58 +0100

    Define API sub namespaces

---
+++ app/app/Http/Controllers/V1/Orgs/OrgController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\V1\Orgs;
+
+use App\Http\Controllers\V1\Controller;
+
+class OrgController extends Controller
+{
+  public function index()
+  {
+    echo 'orgs';
+  }
+}
---
+++ app/app/Http/Controllers/V1/Users/CreateUserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\V1\Users;
+
+use App\Http\Controllers\V1\Controller;
+use App\Http\Requests\V1\CreateUser;
+
+class CreateUserController extends Controller
+{
+  public function create(CreateUser $request)
+  {
+    
+  }
+}
