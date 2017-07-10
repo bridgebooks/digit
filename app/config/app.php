db9ddec52252f95bc395a51475509f4118d12a0d
Patrick <patrickfoh@gmail.com>
2017-07-10 07:15:47 +0100

    Update service providers

--- app/config/app.php
+++ app/config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         JeroenG\Packager\PackagerServiceProvider::class,
+        Mrfoh\Mulla\MullaServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
 
