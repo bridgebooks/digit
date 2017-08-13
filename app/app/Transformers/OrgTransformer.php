3146dd465e234652db9ef4f0bcf54bbafeb27ffb
Patrick <patrickfoh@gmail.com>
2017-08-13 13:30:20 +0100

    Update OrgTransformer

--- app/app/Transformers/OrgTransformer.php
+++ app/app/Transformers/OrgTransformer.php
@@ -9,9 +9,21 @@ use App\Models\Org;
  * Class OrgTransformer
  * @package namespace App\Transformers;
  */
-class OrgTansformer extends TransformerAbstract
+class OrgTransformer extends TransformerAbstract
 {
+    protected $availableIncludes = ['industry'];
 
+    /**
+     * Include Industry
+     * @param Org $org
+     * @return \League\Fractal\Resource\Item
+     */
+    public function includeIndustry(Org $org)
+    {
+        $industry = $org->industry;
+
+        return $this->item($industry, new IndustryTransformer);
+    }
     /**
      * Transform the \Org entity
      * @param \Org $model
@@ -23,7 +35,20 @@ class OrgTansformer extends TransformerAbstract
         return [
             'id' => $model->id,
             'name' => $model->name,
+            'logo_url' => $model->logo_url,
             'business_name' => $model->business_name,
+            'business_reg_no' => $model->business_reg_no,
+            'description' => $model->description,
+            'industry_id' => $model->industry_id,
+            'address_line_1' => $model->address_line_1,
+            'address_line_2' => $model->address_line_2,
+            'city_town' => $model->city_town,
+            'state_region' => $model->state_region,
+            'postal_zip' => $model->postal_zip,
+            'country' => $model->country,
+            'phone' => $model->phone,
+            'email' => $model->email,
+            'website' => $model->website,
             'created_at' => $model->created_at,
             'updated_at' => $model->updated_at
         ];
