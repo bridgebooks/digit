062d79e6d7ebd8989174e9e4f7fb07e0383b6364
Patrick <patrickfoh@gmail.com>
2017-08-13 13:28:53 +0100

    Update org routes

--- app/routes/api/orgs.php
+++ app/routes/api/orgs.php
@@ -2,3 +2,6 @@
 
 Route::get('/', 'OrgController@index');
 Route::post('/', 'OrgController@create');
+Route::post('/logo','OrgController@uploadLogo');
+Route::get('/{id}', 'OrgController@one');
+Route::put('/{id}', 'OrgController@update');
