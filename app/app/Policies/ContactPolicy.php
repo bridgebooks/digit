26aaaf65a3046fa3af3e7471c633215f39eef514
Patrick <patrickfoh@gmail.com>
2017-08-20 11:03:52 +0100

    Add ContactPolicy

---
+++ app/app/Policies/ContactPolicy.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Policies;
+
+use App\Traits\UserRequest;
+use App\Models\User;
+use App\Models\Contact;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ContactPolicy
+{
+    use HandlesAuthorization, UserRequest;
+
+    /**
+     * Determine whether the user can view the appModelsContact.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Contact $contact
+     * @return mixed
+     */
+    public function view(User $user, Contact $contact)
+    {
+        return $user->id === $contact->user_id || $this->belongsToOrg($contact->org_id);
+    }
+
+    /**
+     * Determine whether the user can create Contact.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the contact.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Contact $contact
+     * @return mixed
+     */
+    public function update(User $user, Contact $contact)
+    {
+        return $this->belongsToOrg($contact->org_id);
+    }
+
+    /**
+     * Determine whether the user can delete the Contact.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Contact $contact
+     * @return mixed
+     */
+    public function delete(User $user, Contact $contact)
+    {
+        $belongsToOrg = $this->belongsToOrg($contact->org_id);
+        $isOrgAdmin = $this->userHasRole('org_admin');
+        $isOrgMember = $this->userHasRole('org_member');
+        $isOwner = $contact->user_id === $user->id;
+
+        if ($belongsToOrg) {
+            if ($isOrgAdmin || $isOrgMember && $isOwner) {
+                return true;
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+}
