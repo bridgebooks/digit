c2b93b5da65302bc86f61fee7df2b52310516790
Patrick <patrickfoh@gmail.com>
2017-08-20 11:08:12 +0100

    Update Form Requests

---
+++ app/app/Http/Requests/V1/CreateContact.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use App\Traits\UserRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateContact extends FormRequest
+{
+    use UserRequest;
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if ($_REQUEST)
+        return $this->userHasRole('org_admin') && $this->belongsToOrg($this->request->get('org_id'));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'org_id' => 'required',
+            'email' => 'email',
+            'type' => 'required|in:customer,supplier,vendor'
+        ];
+    }
+}
