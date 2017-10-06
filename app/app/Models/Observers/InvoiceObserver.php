8f19aee442a40edf0349dda8fd0b140c61ffb8df
Patrick <patrickfoh@gmail.com>
2017-10-06 07:02:13 +0100

    Bootstrap observer for invoice model

---
+++ app/app/Models/Observers/InvoiceObserver.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Observers;
+
+use Log;
+use App\Models\Invoice;
+
+class InvoiceObserver
+{
+    /**
+     * Listen to the Invoice created event.
+     *
+     * @param  Invoice  $user
+     * @return void
+     */
+    public function created(Invoice $invoice)
+    {
+        
+    }
+
+    /**
+     * Listen to the Invoice updated event.
+     *
+     * @param  Invoice  $user
+     * @return void
+     */
+    public function updated(Invoice $invoice)
+    {
+        Log::info('Invoice updated', ['status' => $invoice->status ]);
+    }
+}
