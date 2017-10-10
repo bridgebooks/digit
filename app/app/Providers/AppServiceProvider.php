caa5dd428a8806a587e996b2e73627dac73921aa
Patrick <patrickfoh@gmail.com>
2017-10-10 09:09:35 +0100

    Added query logging

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -16,6 +16,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if ($this->app->environment('local')) {
+            \DB::listen(function($query) {
+                $dbLog = new \Monolog\Logger('Query');
+                $dbLog->pushHandler(new \Monolog\Handler\RotatingFileHandler(storage_path('logs/Query.log'), 5, \Monolog\Logger::DEBUG));
+                $dbLog->info($query->sql, ['Bindings' => $query->bindings, 'Time' => $query->time]);
+            });
+        }
     }
 
     /**
