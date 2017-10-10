f7df2a88fa059ff699535de7675c5132c6ff6b19
Patrick <patrickfoh@gmail.com>
2017-10-10 09:09:10 +0100

    Updated listener

--- app/app/Listeners/OrgCreatedListener.php
+++ app/app/Listeners/OrgCreatedListener.php
@@ -2,6 +2,7 @@
 
 namespace App\Listeners;
 
+use App\Models\OrgInvoiceSetting;
 use App\Events\OrgCreated;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -33,8 +34,11 @@ class OrgCreatedListener implements ShouldQueue
         $taxRatesSeeder = new TaxRatesTableSeeder($event->org);
         $accountsSeeder = new AccountsTableSeeder($event->org);
 
+        // run seeders
         $taxRatesSeeder->run();
         $accountsSeeder->run();
 
+        // create invoice settings
+        $settings = new OrgInvoiceSetting(['org_id' => $event->org->id]);
     }
 }
