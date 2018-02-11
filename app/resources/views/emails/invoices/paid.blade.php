1bfde3c08b15408eaaad83a24eecb9a341bfc5d8
Patrick Foh <patrickfoh@gmail.com>
2018-02-11 09:08:34 +0100

    Add payment notification email template

---
+++ app/resources/views/emails/invoices/paid.blade.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!--[if gte mso 9]><xml>
+        <o:OfficeDocumentSettings>
+            <o:AllowPNG/>
+            <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+    </xml><![endif]-->
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <meta content="width=device-width" name="viewport"><!--[if !mso]><!-->
+    <meta content="IE=edge" http-equiv="X-UA-Compatible"><!--<![endif]-->
+    <title></title><!--[if !mso]><!== -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css"><!--<![endif]-->
+
+    <style id="media-query" type="text/css">
+        body {
+            margin: 0;
+            padding: 0; }
+
+        table, tr, td {
+            vertical-align: top;
+            border-collapse: collapse; }
+
+        .ie-browser table, .mso-container table {
+            table-layout: fixed; }
+
+        * {
+            line-height: inherit; }
+
+        a[x-apple-data-detectors=true] {
+            color: inherit !important;
+            text-decoration: none !important; }
+
+        [owa] .img-container div, [owa] .img-container button {
+            display: block !important; }
+
+        [owa] .fullwidth button {
+            width: 100% !important; }
+
+        [owa] .block-grid .col {
+            display: table-cell;
+            float: none !important;
+            vertical-align: top; }
+
+        .ie-browser .num12, .ie-browser .block-grid, [owa] .num12, [owa] .block-grid {
+            width: 500px !important; }
+
+        .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {
+            line-height: 100%; }
+
+        .ie-browser .mixed-two-up .num4, [owa] .mixed-two-up .num4 {
+            width: 164px !important; }
+
+        .ie-browser .mixed-two-up .num8, [owa] .mixed-two-up .num8 {
+            width: 328px !important; }
+
+        .ie-browser .block-grid.two-up .col, [owa] .block-grid.two-up .col {
+            width: 250px !important; }
+
+        .ie-browser .block-grid.three-up .col, [owa] .block-grid.three-up .col {
+            width: 166px !important; }
+
+        .ie-browser .block-grid.four-up .col, [owa] .block-grid.four-up .col {
+            width: 125px !important; }
+
+        .ie-browser .block-grid.five-up .col, [owa] .block-grid.five-up .col {
+            width: 100px !important; }
+
+        .ie-browser .block-grid.six-up .col, [owa] .block-grid.six-up .col {
+            width: 83px !important; }
+
+        .ie-browser .block-grid.seven-up .col, [owa] .block-grid.seven-up .col {
+            width: 71px !important; }
+
+        .ie-browser .block-grid.eight-up .col, [owa] .block-grid.eight-up .col {
+            width: 62px !important; }
+
+        .ie-browser .block-grid.nine-up .col, [owa] .block-grid.nine-up .col {
+            width: 55px !important; }
+
+        .ie-browser .block-grid.ten-up .col, [owa] .block-grid.ten-up .col {
+            width: 50px !important; }
+
+        .ie-browser .block-grid.eleven-up .col, [owa] .block-grid.eleven-up .col {
+            width: 45px !important; }
+
+        .ie-browser .block-grid.twelve-up .col, [owa] .block-grid.twelve-up .col {
+            width: 41px !important; }
+
+        @media only screen and (min-width: 520px) {
+            .block-grid {
+                width: 500px !important; }
+            .block-grid .col {
+                display: table-cell;
+                Float: none !important;
+                vertical-align: top; }
+            .block-grid .col.num12 {
+                width: 500px !important; }
+            .block-grid.mixed-two-up .col.num4 {
+                width: 164px !important; }
+            .block-grid.mixed-two-up .col.num8 {
+                width: 328px !important; }
+            .block-grid.two-up .col {
+                width: 250px !important; }
+            .block-grid.three-up .col {
+                width: 166px !important; }
+            .block-grid.four-up .col {
+                width: 125px !important; }
+            .block-grid.five-up .col {
+                width: 100px !important; }
+            .block-grid.six-up .col {
+                width: 83px !important; }
+            .block-grid.seven-up .col {
+                width: 71px !important; }
+            .block-grid.eight-up .col {
+                width: 62px !important; }
+            .block-grid.nine-up .col {
+                width: 55px !important; }
+            .block-grid.ten-up .col {
+                width: 50px !important; }
+            .block-grid.eleven-up .col {
+                width: 45px !important; }
+            .block-grid.twelve-up .col {
+                width: 41px !important; } }
+
+        @media (max-width: 520px) {
+            .block-grid, .col {
+                min-width: 320px !important;
+                max-width: 100% !important; }
+            .block-grid {
+                width: calc(100% - 40px) !important; }
+            .col {
+                width: 100% !important; }
+            .col > div {
+                margin: 0 auto; }
+            img.fullwidth {
+                max-width: 100% !important; } }
+
+    </style>
+</head>
+<body class="clean-body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #FFFFFF">
+<!--[if IE]><div class="ie-browser"><![endif]-->
+<!--[if mso]><div class="mso-container"><![endif]-->
+<div class="nl-container" style="min-width: 320px;Margin: 0 auto;background-color: #FFFFFF">
+    <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td align="center" style="background-color: #FFFFFF;"><![endif]-->
+    <div style="background-color:transparent;">
+        <div class="block-grid" style="Margin: 0 auto;min-width: 320px;max-width: 500px;width: 500px;width: calc(19000% - 98300px);overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
+            <div style="border-collapse: collapse;display: table;width: 100%;">
+                <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="background-color:transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width: 500px;"><tr class="layout-full-width" style="background-color:transparent;"><![endif]-->
+                <!--[if (mso)|(IE)]><td align="center" width="500" style=" width:500px; padding-right: 0px; padding-left: 0px; padding-top:5px; padding-bottom:5px; border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent;" valign="top"><![endif]-->
+                <div class="col num12" style="min-width: 320px;max-width: 500px;width: 500px;width: calc(18000% - 89500px);background-color: transparent;">
+                    <div style="background-color: transparent; width: 100% !important;">
+                        <!--[if (!mso)&(!IE)]><!-->
+                        <div style="border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent; padding-top:5px; padding-bottom:5px; padding-right: 0px; padding-left: 0px;">
+                            <!--<![endif]-->
+                            &#160;
+                            <div style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 10px;">
+                                <!--[if (mso)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px;padding-left: 10px; padding-top: 10px; padding-bottom: 10px;"><table width="100%" align="center" cellpadding="0" cellspacing="0" border="0"><tr><td><![endif]-->
+                                <div align="center">
+                                    <div style="border-top: 1px solid #BBBBBB; width:100%; line-height:1px; height:1px; font-size:1px;">
+                                        &#160;
+                                    </div>
+                                </div><!--[if (mso)]></td></tr></table></td></tr></table><![endif]-->
+                            </div><!--[if (!mso)&(!IE)]><!-->
+                        </div><!--<![endif]-->
+                    </div>
+                </div><!--[if (mso)|(IE)]></td></tr></table></td></tr></table><![endif]-->
+            </div>
+        </div>
+    </div>
+    <div style="background-color:transparent;">
+        <div class="block-grid" style="Margin: 0 auto;min-width: 320px;max-width: 500px;width: 500px;width: calc(19000% - 98300px);overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
+            <div style="border-collapse: collapse;display: table;width: 100%;">
+                <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="background-color:transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width: 500px;"><tr class="layout-full-width" style="background-color:transparent;"><![endif]-->
+                <!--[if (mso)|(IE)]><td align="center" width="500" style=" width:500px; padding-right: 0px; padding-left: 0px; padding-top:20px; padding-bottom:20px; border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent;" valign="top"><![endif]-->
+                <div class="col num12" style="min-width: 320px;max-width: 500px;width: 500px;width: calc(18000% - 89500px);background-color: transparent;">
+                    <div style="background-color: transparent; width: 100% !important;">
+                        <!--[if (!mso)&(!IE)]><!-->
+                        <div style="border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent; padding-top:20px; padding-bottom:20px; padding-right: 0px; padding-left: 0px;">
+                            <!--<![endif]-->
+                            <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 10px;"><![endif]-->
+                            <div style="color:#0068A5;line-height:120%;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif; padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 10px;">
+                                <div style="font-size:16px;line-height:18px;text-align:left;color:#000;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;">
+                                    <p style="margin: 0;font-size: 16px;line-height: 18px;text-align: left">
+                                        Hello,
+                                    </p>
+                                    <p style="margin: 0;font-size: 16px;line-height: 18px;text-align: left">
+                                        Your invoice {{ $invoice->invoice_no }} with a total NGN {{  number_format($invoice->total,2) }} has been paid. The invoice status will automatically updated.
+                                    </p>
+                                    <p style="margin: 0;font-size: 16px;line-height: 18px;text-align: left">
+                                       Thank you for using Bridgebooks.
+                                    </p>
+                                </div>
+                            </div><!--[if mso]></td></tr></table><![endif]-->
+                            <div align="center" class="button-container center" style="padding-right: 10px; padding-left: 10px; padding-top:10px; padding-bottom:10px;">
+                                <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0" style="border-spacing: 0; border-collapse: collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top:10px; padding-bottom:10px;" align="center"><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="localhost:4040/login/validate/08d5cea0-69f4-11e7-9ac3-a3972359d417" style="height:42px; v-text-anchor:middle; width:220px;" arcsize="10%" strokecolor="#0068A5" fillcolor="#0068A5"><w:anchorlock/><center style="color:#ffffff; font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size:16px;"><![endif]-->
+                                <a href="{{ $params['url'] }}" target="_blank" style="display: inline-block;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #ffffff; background-color: #0068A5; border-radius: 4px; -webkit-border-radius: 4px; -moz-border-radius: 4px; max-width: 200px; width: 140px; width: auto; border-top: 0px solid transparent; border-right: 0px solid transparent; border-bottom: 0px solid transparent; border-left: 0px solid transparent; padding-top: 5px; padding-right: 30px; padding-bottom: 5px; padding-left: 30px; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif;mso-border-alt: none" target="_blank"><span style="font-size:12px;line-height:24px;"><span data-mce-style="font-size: 16px;" style="font-size: 16px; line-height: 32px;">View Invoice</span></span></a> <!--[if mso]></center></v:roundrect></td></tr></table><![endif]-->
+                            </div>
+                            <div style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 10px;">
+                                <!--[if (mso)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px;padding-left: 10px; padding-top: 10px; padding-bottom: 10px;"><table width="100%" align="center" cellpadding="0" cellspacing="0" border="0"><tr><td><![endif]-->
+                                <div align="center">
+                                    <div style="border-top: 1px solid #BBBBBB; width:100%; line-height:1px; height:1px; font-size:1px;">
+                                        &#160;
+                                    </div>
+                                </div><!--[if (mso)]></td></tr></table></td></tr></table><![endif]-->
+                            </div><!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 10px;"><![endif]-->
+                            <div style="color:#555555;line-height:120%;font-family:'Lato', Tahoma, Verdana, Segoe, sans-serif; padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 10px;">
+                                <div style="font-size:12px;line-height:14px;font-family:Lato, Tahoma, Verdana, Segoe, sans-serif;color:#555555;text-align:left;">
+                                    <p style="margin: 0;font-size: 12px;line-height: 14px"><span style="font-size: 14px; line-height: 16px;">If you have any questions you can contact us at <a href="mailto:support@bridgebooks.ng">support@bridebooks.ng</a></span></p>
+                                </div>
+                            </div><!--[if mso]></td></tr></table><![endif]-->
+                            <!--[if (!mso)&(!IE)]><!-->
+                        </div><!--<![endif]-->
+                    </div>
+                </div><!--[if (mso)|(IE)]></td></tr></table></td></tr></table><![endif]-->
+            </div>
+        </div>
+    </div><!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+</div><!--[if (mso)|(IE)]></div><![endif]-->
+</body>
+</html>
