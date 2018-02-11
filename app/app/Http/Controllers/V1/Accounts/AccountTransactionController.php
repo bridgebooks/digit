88844f6442ff8eb15dcc898d1dae46d4ae42d5b3
Patrick Foh <patrickfoh@gmail.com>
2018-02-11 09:07:08 +0100

    Update event

---
+++ app/app/Http/Controllers/V1/Accounts/AccountTransactionController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 10/02/2018
+ * Time: 08:51
+ */
+
+namespace App\Http\Controllers\V1\Accounts;
+
+
+class AccountTransactionController
+{
+
+}
