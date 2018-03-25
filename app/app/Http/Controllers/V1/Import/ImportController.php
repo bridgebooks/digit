69fb21a5bee553df452282eec2b2f4f7ddb3d269
Patrick Foh <patrickfoh@gmail.com>
2018-03-25 16:03:56 +0100

    Add import routes

---
+++ app/app/Http/Controllers/V1/Import/ImportController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 25/03/2018
+ * Time: 11:09
+ */
+
+namespace App\Http\Controllers\V1\Import;
+
+
+class ImportController
+{
+
+}
---
+++ app/app/Services/Imports/CSVImportService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 25/03/2018
+ * Time: 13:12
+ */
+
+namespace App\Services\Imports;
+
+
+class CSVImportService
+{
+
+}
