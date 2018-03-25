2aae9e2b7260ee401467f554e45c21e7b41173bd
Patrick Foh <patrickfoh@gmail.com>
2018-03-25 16:05:40 +0100

    Add ImportController

--- app/app/Http/Controllers/V1/Import/ImportController.php
+++ app/app/Http/Controllers/V1/Import/ImportController.php
@@ -1,15 +1,72 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 25/03/2018
- * Time: 11:09
- */
 
 namespace App\Http\Controllers\V1\Import;
 
+use App\Http\Controllers\V1\Controller;
+use App\Repositories\ContactRepository;
+use App\Repositories\EmployeeRepository;
+use App\Services\Imports\CSVImportService;
+use App\Traits\UserRequest;
+use Illuminate\Http\Request;
+use League\Csv\Exception;
 
-class ImportController
+class ImportController extends Controller
 {
+    use UserRequest;
 
+    protected $contacts;
+    protected $employees;
+
+    public function __construct(ContactRepository $contacts, EmployeeRepository $employees)
+    {
+        $this->middleware('jwt.auth');
+        $this->contacts = $contacts;
+        $this->employees = $employees;
+    }
+
+    /**
+     * Get Repository
+     * @param string $name
+     * @return ContactRepository|EmployeeRepository
+     */
+    private function getRepository(string $name)
+    {
+        switch ($name) {
+            case 'contacts':
+                return $this->contacts;
+                break;
+            case 'employees':
+                return $this->employees;
+                break;
+        }
+    }
+
+    public function process(Request $request)
+    {
+        $file = $request->file('file');
+        $repositoryName = $request->input('repository', 'contacts');
+        $org = $request->input('org_id');
+        $repository = $this->getRepository($repositoryName);
+
+        $name = md5(rand(0, time())).'.csv';
+        $file->storeAs('tmp/imports', $name);
+
+        $csvImporter = new CSVImportService($name, $repository, $this->requestUser());
+
+        try {
+            $csvImporter
+                ->setOrgID($org)
+                ->process();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Data imported successfully'
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Error parsing csv file'
+            ]);
+        }
+    }
 }
